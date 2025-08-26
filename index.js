0825d182b0829fdf61b0303a647c2d697d339cc4
Amit Shirasao <amit.shirasao@gmail.com>
2025-08-26 12:17:56 +0530

    Node JS code is removed.

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-// const http = require('http');
 const express = require('express');
 
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello, India!\n');
-// });
 const app = express();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-// server.listen(3000, 'localhost', () => {
-//   console.log('Server running at http://localhost:3000/');
-// });
 app.listen(4000, () => {
-  console.log('Express app listening on port 4000');
+    console.log('Express app listening on port 4000');
 });
 
 app.get('/', (req, res) => {
-  res.send('Get all employees.');
+    res.send('Get all employees.');
 });
