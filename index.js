28d2858474d508533b5e050ca585fdec3a97946c
Amit Shirasao <amit.shirasao@gmail.com>
2025-08-26 12:29:46 +0530

    Express JS was used to create API calls. They were tested using Postman.

--- index.js
+++ index.js
@@ -12,3 +12,24 @@ app.listen(4000, () => {
 app.get('/', (req, res) => {
     res.send('Get all employees.');
 });
+
+app.get('/:id', (req, res) => {
+    const { id } = req.params;      
+    res.send(`Get employee with ID: ${id}`);
+});
+
+app.post('/', (req, res) => {
+    const newEmployee = req.body;
+    res.send(`Add new employee: ${JSON.stringify(newEmployee)}`);
+});
+
+app.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const updatedEmployee = req.body;
+    res.send(`Update employee with ID ${id}: ${JSON.stringify(updatedEmployee)}`);
+});
+
+app.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    res.send(`Delete employee with ID: ${id}`);
+});
